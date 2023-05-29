d09e5254226dd98bea0b59bfd01212e08ddcc288
viktor <viktor@ubuntu.myguest.virtualbox.org>
2023-05-29 14:35:17 +0200

    Use only y zoom

--- src/pages/Stats/components/LineChart.tsx
+++ src/pages/Stats/components/LineChart.tsx
@@ -55,7 +55,7 @@ const LineChart = (props: { data: SnapshotMemorySeries[] }) => {
               pinch: {
                 enabled: true,
               },
-              mode: "xy",
+              mode: "y",
             },
           },
         },
